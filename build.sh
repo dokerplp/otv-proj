9e5e8cc6fdf2056fe8a831972d940b3c8da7aaea
Valerii Butorin <butorin.v.a@mail.ru>
2023-12-10 15:12:41 +0300

    build

--- build.sh
+++ build.sh
@@ -7,4 +7,11 @@ echo "[default]" > ~/.aws/credentials
 echo "  aws_access_key_id = $AWS_ACCESS_KEY" >> ~/.aws/credentials
 echo "  aws_secret_access_key = $AWS_SECRET_KEY" >> ~/.aws/credentials
 
+mkdir -p ~/.postgresql && \
+wget "https://storage.yandexcloud.net/cloud-certs/CA.pem" \
+    --output-document ~/.postgresql/root.crt && \
+chmod 0600 ~/.postgresql/root.crt
+
+sudo apt update && sudo apt install --yes default-jdk maven
+
 ./gradlew bootRun
